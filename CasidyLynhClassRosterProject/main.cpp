18ca030a0e2d64d7893d9cbfdb893341e372f03d
themother0fdogs <110417160+themother0fdogs@users.noreply.github.com>
2022-12-11 17:01:04 -0500

    Update main.cpp

--- CasidyLynhClassRosterProject/main.cpp
+++ CasidyLynhClassRosterProject/main.cpp
@@ -15,16 +15,18 @@ int main()
 	const string studentData[] =
 	{
 			"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
-			"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
+			"A2,Elle,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
 			"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
 			"A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-			"A5,Myfirstname,Mylastname,myemail@gmail.com,27, 20, 20, 20,SOFTWARE"
+			"A5,Lynh,Casidy,lcasidy@wgu.edu,27, 20, 20, 20,SOFTWARE"
 	};
 
 	Roster* classRoster = new Roster(numStudents);
 	for (int i = 0; i < numStudents; i++) {
 		classRoster->parseAndAdd(studentData[i]);
 	}
+	cout << "Class Roster Application" << endl;
+	cout << "-------------------------------------------------------------------------------" << endl;
 
 	cout << "Displaying all students: " << endl <<endl;
 	classRoster->printAll();
